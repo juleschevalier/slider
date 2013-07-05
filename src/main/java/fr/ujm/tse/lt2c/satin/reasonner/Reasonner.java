04b51f2a41176adb30778e9beb70e4a7d595a879
Jules Chevalier <jules.chevalier@univ-st-etienne.fr>
2013-07-05 15:57:17 +0200

    Add EQ{DIFF1,REF,REP_O,REP_P,REP_S,SYM,TRANS,},PRP{EQP1,EQP2,INV1,INV2,PDW,},SCM{EQC1,EQP1,}\nOptimisations : SCM_{EQC2,EQP2} avoid A equivalent{Class,Property} A

--- src/main/java/fr/ujm/tse/lt2c/satin/reasonner/Reasonner.java
+++ src/main/java/fr/ujm/tse/lt2c/satin/reasonner/Reasonner.java
@@ -9,12 +9,20 @@ import fr.ujm.tse.lt2c.satin.interfaces.TripleStore;
 import fr.ujm.tse.lt2c.satin.naiveImpl.DictionnaryImplNaive;
 import fr.ujm.tse.lt2c.satin.naiveImpl.ParserImplNaive;
 import fr.ujm.tse.lt2c.satin.naiveImpl.TripleStoreImplNaive;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleEQ_DIFF1;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_DOM;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_EQP1;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_EQP2;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_INV1;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_INV2;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_PDW;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_RNG;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RulePRP_SPO1;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_DOM1;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_DOM2;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_EQC1;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_EQC2;
+import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_EQP1;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_EQP2;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_RNG1;
 import fr.ujm.tse.lt2c.satin.naiveImpl.rules.RuleSCM_RNG2;
@@ -35,26 +43,51 @@ public class Reasonner {
 		
 		ArrayList<Rule> rules = new ArrayList<>();
 
+		//range
 		rules.add(new RulePRP_RNG());
+		//domain
 		rules.add(new RulePRP_DOM());
+		//subClassOf
 		rules.add(new RuleSCM_SCO());
 		rules.add(new RuleSCM_EQC2());
 		rules.add(new RuleSCM_RNG1());
 		rules.add(new RuleSCM_DOM1());
+		//subPropertyOf
 		rules.add(new RulePRP_SPO1());
 		rules.add(new RuleSCM_SPO());
 		rules.add(new RuleSCM_DOM2());
 		rules.add(new RuleSCM_RNG2());
 		rules.add(new RuleSCM_EQP2());
-
+		//equivalentProperty
+		rules.add(new RulePRP_EQP1());
+		rules.add(new RulePRP_EQP2());
+		rules.add(new RuleSCM_EQP1());
+		//inverseOf
+		rules.add(new RulePRP_INV1());
+		rules.add(new RulePRP_INV2());
+		//sameAs
+//		rules.add(new RuleEQ_REF());
+//		rules.add(new RuleEQ_SYM());
+//		rules.add(new RuleEQ_TRANS());
+//		rules.add(new RuleEQ_REP_S());
+//		rules.add(new RuleEQ_REP_P());
+//		rules.add(new RuleEQ_REP_O());
+		//equivalentClass
+		rules.add(new RuleSCM_EQC1());
+		//differentFrom
+		rules.add(new RuleEQ_DIFF1());
+		//propertyDisjointWith
+		rules.add(new RulePRP_PDW());
+		
+		
 		for (Rule rule : rules) {
-			rule.process(tripleStore, dictionnary);			
+			rule.process(tripleStore, dictionnary);
 		}
 		
 //		for (Triple triple : tripleStore.getAll()) {
 //			System.out.println(dictionnary.printTriple(triple));
 //		}
-
+		System.out.println("Entrées dictionnaire : "+dictionnary.size());
 		System.out.println("Triples avant : "+beginNbTriples);
 		System.out.println("Triples après : "+tripleStore.getAll().size());
 		System.out.println("Triples générés : "+(tripleStore.getAll().size()-beginNbTriples));
