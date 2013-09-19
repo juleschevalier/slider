e7fbea194133cb3390e2cbababe23beffd14e8d5
Jules Chevalier <jules.chevalier@univ-st-etienne.fr>
2013-09-19 17:13:56 +0200

    Track renamed files

---
+++ src/main/java/fr/ujm/tse/lt2c/satin/dictionary/AbstractDictionary.java
@@ -0,0 +1,58 @@
+package fr.ujm.tse.lt2c.satin.dictionary;
+
+import fr.ujm.tse.lt2c.satin.interfaces.Dictionary;
+
+
+/**
+ * @author Jules Chevalier
+ *
+ */
+public abstract class AbstractDictionary implements Dictionary{
+
+	public static long wedontcare = -1;
+	public static long allDifferent = 0;
+	public static long allDisjointClasses = 0;
+	public static long allValuesFrom = 0;
+	public static long annotationProperty = 0;
+	public static long assertionProperty = 0;
+	public static long asymetricProperty = 0;
+	public static long clazz = 0;
+	public static long complementOf = 0;
+	public static long differentFrom = 0;
+	public static long disjoinWith = 0;
+	public static long distinctmembers = 0;
+	public static long equivalentClass = 0;
+	public static long equivalentProperty = 0;
+	public static long functionalProperty = 0;
+	public static long hasKey = 0;
+	public static long hasValue = 0;
+	public static long intersectionOf = 0;
+	public static long inverseFunctionalProperty = 0;
+	public static long inverseOf = 0;
+	public static long irreflexiveProperty = 0;
+	public static long maxCardinality = 0;
+	public static long maxQualifiedCardinality = 0;
+	public static long members = 0;
+	public static long nothing = 0;
+	public static long onClass = 0;
+	public static long oneOf = 0;
+	public static long onProperty = 0;
+	public static long propertyChainAxiom = 0;
+	public static long propertyDisjointWith = 0;
+	public static long sameAs = 0;
+	public static long someValuesFrom = 0;
+	public static long sourceIndividual = 0;
+	public static long symetricProperty = 0;
+	public static long targetIndividual = 0;
+	public static long targetValue = 0;
+	public static long thing = 0;
+	public static long transitiveProperty = 0;
+	public static long unionOf = 0;
+	public static long domain = 0;
+	public static long range = 0;
+	public static long subClassOf = 0;
+	public static long subProperty = 0;
+	public static long subPropertyOf = 0;
+	public static long type = 0;
+	
+}
---
+++ src/main/java/fr/ujm/tse/lt2c/satin/dictionary/DictionaryImplNaive.java
@@ -0,0 +1,147 @@
+package fr.ujm.tse.lt2c.satin.dictionary;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import fr.ujm.tse.lt2c.satin.interfaces.Triple;
+
+
+/**
+ * @author Jules Chevalier
+ *
+ */
+public class DictionaryImplNaive extends AbstractDictionary{
+
+	private HashMap<String,Long> triples= new HashMap<>();
+	long counter;
+
+	
+	public DictionaryImplNaive() {
+		super();
+		this.triples = new HashMap<>();
+		this.counter = 0;
+		wedontcare = add("X");
+		//RhoDF
+		domain = add("http://www.w3.org/2000/01/rdf-schema#domain");
+		range = add("http://www.w3.org/2000/01/rdf-schema#range");
+		type = add("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+		subClassOf = add("http://www.w3.org/2000/01/rdf-schema#subClassOf");
+		subPropertyOf = add("http://www.w3.org/2000/01/rdf-schema#subPropertyOf");
+		equivalentClass = add("http://www.w3.org/2002/07/owl#equivalentClass");
+		equivalentProperty = add("http://www.w3.org/2002/07/owl#equivalentProperty");
+		sameAs = add("http://www.w3.org/2002/07/owl#sameAs");
+		
+		inverseOf = add("http://www.w3.org/2002/07/owl#inverseOf");
+		propertyDisjointWith = add("");
+		differentFrom = add("");
+		allDifferent = add("http://www.w3.org/2002/07/owl#AllDifferent");
+		allDisjointClasses = add("");
+		allValuesFrom = add("http://www.w3.org/2002/07/owl#allValuesFrom");
+		annotationProperty = add("http://www.w3.org/2002/07/owl#AnnotationProperty");
+		assertionProperty = add("");
+		asymetricProperty = add("");
+		clazz = add("http://www.w3.org/2002/07/owl#Class");
+		complementOf = add("");
+		disjoinWith = add("http://www.w3.org/2002/07/owl#disjointWith");
+		distinctmembers = add("http://www.w3.org/2002/07/owl#distinctMembers");
+		functionalProperty = add("");
+		hasKey = add("");
+		hasValue = add("");
+		intersectionOf = add("");
+		inverseFunctionalProperty = add("");
+		irreflexiveProperty = add("");
+		maxCardinality = add("http://www.w3.org/2002/07/owl#maxCardinality");
+		maxQualifiedCardinality = add("");
+		members = add("");
+		nothing = add("");
+		onClass = add("");
+		onProperty = add("http://www.w3.org/2002/07/owl#onProperty");
+		oneOf = add("");
+		propertyChainAxiom = add("");
+		someValuesFrom = add("");
+		sourceIndividual = add("");
+		symetricProperty = add("");
+		targetIndividual = add("");
+		targetValue = add("");
+		thing = add("http://www.w3.org/2002/07/owl#Thing");
+		transitiveProperty = add("http://www.w3.org/2002/07/owl#TransitiveProperty");
+		unionOf = add("");
+	}
+
+	@Override
+	public long add(String s) {
+		if(this.triples.containsKey(s)){
+			return this.get(s);
+		}
+		this.triples.put(s, this.counter);
+		return this.counter++;
+	}
+
+	@Override
+	public String get(long index) {
+		Iterator<Entry<String, Long>> it = this.triples.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<String, Long> pairs = (Entry<String, Long>) it.next();
+			if (pairs.getValue().equals(index))
+				return pairs.getKey();
+		}
+		return null;
+	}
+
+	@Override
+	public long get(String s) {
+		return this.triples.get(s);
+	}
+
+	@Override
+	public long size() {
+		return this.triples.size();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (counter ^ (counter >>> 32));
+		result = prime * result + ((triples == null) ? 0 : triples.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DictionaryImplNaive other = (DictionaryImplNaive) obj;
+		if (counter != other.counter)
+			return false;
+		if (triples == null) {
+			if (other.triples != null)
+				return false;
+		} else if (!triples.equals(other.triples))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String printTriple(Triple t){
+		String s = this.get(t.getSubject()),
+			   p = this.get(t.getPredicate()),
+			   o = this.get(t.getObject());
+		
+		if(s.split("#").length>1)
+			s=s.split("#")[1];
+		if(p.split("#").length>1)
+			p=p.split("#")[1];
+		if(o.split("#").length>1)
+			o=o.split("#")[1];
+		
+		return s+" "+p+" "+o;
+	}
+
+}
